6160be2de0d8a8b443aea1445637ad4c891beed3
bg <bg@gmail.com>
2022-09-15 19:41:59 +0530

    versioning...

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='Maplecache',
-    version='1.3',
+    version='1.4',
     description="Simple async 'borrowed' caching system for Python",
     author='Rootspring SkyClan',
     author_email='cheesycod@outlook.com',
