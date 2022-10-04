18540938d512397258b0258bdfa1353dea486c24
bg <bg@gmail.com>
2022-10-04 17:05:50 +0530

    release 1.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='Maplecache',
-    version='1.4',
+    version='1.5',
     description="Simple async 'borrowed' caching system for Python",
     author='Rootspring SkyClan',
     author_email='cheesycod@outlook.com',
